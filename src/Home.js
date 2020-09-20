19a3ea6e4cfe6a3cbbcf0c8207859add433883b9
Akash Dandriyal <akash.d1409@gmail.com>
2020-09-21 00:19:43 +0530

    Home banner complete

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css";
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/CEPC/under1499store/english/Gateway/updated/V242338866_IN_CEPC_Under-1499_store_Graphics_1500x600._CB406499464_.jpg" 
+            alt=""/>
+        </div>
+    )
+}
+
+export default Home
